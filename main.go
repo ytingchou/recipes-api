13b35051d0347ac387210316fe2ba83a7833b09b
Yuting Chou <ytingchou@gmail.com>
2022-06-11 23:58:20 +0800

    recipe data model (#1)
    recipe data model

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
 
 func main() {
 	router := gin.Default()
